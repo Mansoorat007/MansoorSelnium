7f6bb8ba146692d49b800367119db0a7927cb674
Mansoorat007 <47735911+Mansoorat007@users.noreply.github.com>
2019-02-18 08:01:40 +0000

    created a new repository

---
+++ Project1/src/Tca.java
@@ -0,0 +1,21 @@
+
+public class Tca {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String input ="My name is mansoor";
+		
+		char[] a= input.toCharArray(); //input.toCharArray();
+		
+		for(int i=a.length-1;i>=0;i--) {
+			System.out.print(a[i]);
+		}
+			
+		
+		
+		
+				
+
+	}
+
+}
---
+++ Project1/src/Twenty25.java
@@ -0,0 +1,22 @@
+
+public class Twenty25 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String ab = "Where Are you";
+		StringBuilder rv= new StringBuilder();
+		rv.append(ab);
+		
+		rv=rv.reverse();
+		
+		System.out.println("reverse is : "+rv);
+	}
+}
+
+		
+		
+		
+		
+		
+				
