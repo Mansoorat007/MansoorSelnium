e01b2d3b418c47eb616a50bc707218c5c8a650ad
mansoorat007 <mansoor_ahmed007@yahoo.com>
2019-02-18 08:17:09 +0000

    commit git

--- Project1/src/Twenty25.java
+++ Project1/src/Twenty25.java
@@ -4,7 +4,7 @@ public class Twenty25 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String ab = "Where Are you";
+		String ab = "Where Are you gittt";
 		StringBuilder rv= new StringBuilder();
 		rv.append(ab);
 		
